f709cba3847e8eb8c8911fcc11a07fe3d7ae9646
Crew Spence <csspence123@gmail.com>
2020-06-25 06:57:07 -0400

    Wrote working solution.

--- pick_it_codewars.js
+++ pick_it_codewars.js
@@ -4,10 +4,16 @@ Coding in function ```pickIt```, function accept 1 parameter:```arr```, it's a n
 I've defined two array ```odd``` and ```even``` in the function, and also wrote the return statement. ```your work is write a for loop.```
 */
 
-function pickIt(arr){
-  var odd=[],even=[];
-  //coding here
-  
+const pickIt = (arr) => {
+  let odd = [];
+  let even = [];
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] % 2 === 0) {
+      even.push(arr[i]);
+    } else {
+      odd.push(arr[i]);
+    }
+  }
   
   return [odd,even];
 }
